a2ca0f605738a3d834bb6c046447b02b7f3c8ed9
AbdelrahmanHany22 <boudyhany22@gmail.com>
2023-01-07 03:25:56 +0200

    tests & fin

--- sp-reservations-api/tests/k6/reservation/reservation.spec.js
+++ sp-reservations-api/tests/k6/reservation/reservation.spec.js
@@ -1,24 +1,7 @@
-// import http from 'k6/http';
-// import { check, sleep } from 'k6';
-
-// // Configure our test to run 50 virtual users continuously for one minute. 
-// // Because of the sleep added, this will result in just below 50 iterations per second
-// // resulting in a total of about 2900 iterations.
-// export const options = {
-//   duration: '10s',
-//   vus: 50,
-// };
-
-// export default function () {
-//   const res = http.get('http://localhost:3000/api/v1/health');
-//   // assertions
-//   check(res, { 'status was 200': (r) => r.status == 200 });
-//   sleep(1);
-// }
-import axios from 'axios';
-import reservation from '../../../api/index';
-
-const BASE_URL = 'http://localhost:5005'
+const request = require('supertest');
+const axios = require('axios');//import reservation from '../../../api/index';
+const reservationApiStub = require('./reservation.api.Stub.json');
+const BASE_URL = 'https://reservations-wc.vercel.app'
 
 jest.mock('axios')
 
@@ -28,24 +11,80 @@ beforeEach(() => {
 });
 
 describe("Reservation process", ()=>{
-  // Weather Endpoint
-  test('Expected Reservation Response', async () => {
+  // tests
+  test('Expected Reservation Response for good requests to be equal to preset', async () => {
     const data = {"email":"abdo@gmail.com","matchNumber":11,"tickets":{"category":1,"quantity":1,"price":75},
     "card":{"number":"4242 4242 4242 4242","expirationMonth":12,"expirationYear":2023,"cvc":"424"}
   }
+  jest.setTimeout(12000)
   axios.post.mockResolvedValueOnce(data);
-    const response = await request(BASE_URL).post('/api/v1/reservation')
-    .send(data)
-    expect(response.body).toEqual(mockWeatherApiStub.data);
-    expect(getWeather).toHaveBeenCalled();
+    const response = await request(BASE_URL).post('/api/v1/reservation').send(data)
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.doc.price).toEqual(reservationApiStub.data.doc.price);
+    expect(response.body.doc.email).toEqual(reservationApiStub.data.doc.email);
+    expect(response.body.message).toEqual(reservationApiStub.data.message);
   });
-  test('Incorrect Weather Arguments', async () => {
-    const response = await request(BASE_URL).get('/weather?state=istanbul')
-    expect(response.error.text).toContain("Expected city argument");
+
+  test('Expected Reservation Response for Wrong category/price combination', async () => {
+    const data = {"email":"abdo@gmail.com","matchNumber":11,"tickets":{"category":3,"quantity":1,"price":75},
+    "card":{"number":"4242 4242 4242 4242","expirationMonth":12,"expirationYear":2023,"cvc":"424"}
+  }
+  jest.setTimeout(5000)
+  axios.post.mockResolvedValueOnce(data);
+    const response = await request(BASE_URL).post('/api/v1/reservation').send(data)
+
+    expect(response.statusCode).toBe(403);
+
   });
-  test('No Weather Response', async () => {
-    getWeather.mockImplementationOnce((location) => null);
-    const response = await request(BASE_URL).get('/weather?city=Cairo')
-    expect(response.error.text).toContain("Could not process request");
-  }); 
+  
+  test('Expected Reservation Response missing input', async () => {
+    const data = {"email":"","matchNumber":11,"tickets":{"category":1,"quantity":1,"price":75},
+    "card":{"number":"4242 4242 4242 4242","expirationMonth":12,"expirationYear":2023,"cvc":"424"}
+  }
+  jest.setTimeout(5000)
+  axios.post.mockResolvedValueOnce(data);
+    const response = await request(BASE_URL).post('/api/v1/reservation').send(data)
+
+    expect(response.statusCode).toBe(403);
+    expect(response.text).toBe(`"email" is not allowed to be empty`);
+  });
+
+  test('Expected Reservation Response failed stripe', async () => {
+    const data = {"email":"abdo@gmail.com","matchNumber":1,"tickets":{"category":1,"quantity":1,"price":75},
+    "card":{"number":"3535 4242 4242 4242","expirationMonth":12,"expirationYear":2023,"cvc":"424"}
+  }
+  jest.setTimeout(5000)
+  axios.post.mockResolvedValueOnce(data);
+    const response = await request(BASE_URL).post('/api/v1/reservation').send(data)
+    expect(response.statusCode).toBe(400);
+    expect(response.text).toMatch(`could not process payment:`);
+  });
+
+  test('Expected Reservation Response for quantity more than 2', async () => {
+    const data = {"email":"abdo@gmail.com","matchNumber":11,"tickets":{"category":1,"quantity":9,"price":75},
+    "card":{"number":"4242 4242 4242 4242","expirationMonth":12,"expirationYear":2023,"cvc":"424"}
+  }
+  jest.setTimeout(5000)
+  axios.post.mockResolvedValueOnce(data);
+    const response = await request(BASE_URL).post('/api/v1/reservation').send(data)
+    expect(response.statusCode).toBe(403);
+    expect(response.text).toMatch(`"tickets.quantity" must be less than or equal to 2`);
+  });
+
+// get reserved matches endpoint
+
+  test('Expectedresponse for existing email in the db', async () => {
+    const response = await request(BASE_URL).post('/api/v1/tickets/reserved').send({"email":"abdo@gmail.com"})
+    expect(response.body.status).toEqual('success');
+    expect(response.body.data).toBeDefined() ;
+  });
+
+  
+  test('Expectedresponse for none existing email in the db', async () => {
+    const response = await request(BASE_URL).post('/api/v1/tickets/reserved').send({"email":"gogogogog@gmail.com"})
+    expect(response.body.status).toEqual('failed');
+    expect(response.body.data).toEqual('No tickets bought with this email from this website.') ;
+  });
+
 })
